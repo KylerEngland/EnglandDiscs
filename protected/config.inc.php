9b98d59d42d206d0ea7986f9f16e3c3916d89c0f
KylerEngland <80475428+KylerEngland@users.noreply.github.com>
2023-01-31 08:39:44 -0500

    Delete protected directory

--- protected/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('DBHOST', 'localhost');
-define('DBNAME', 'discgolf');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNSTRING', "mysql:host=". DBHOST. ";dbname=". DBNAME);
-
-?>
