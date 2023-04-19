630b982d8a42aaffb7e5e8e834af012b7421c1e9
KylerEngland <80475428+KylerEngland@users.noreply.github.com>
2023-04-19 16:47:54 -0400

    Added a user class for login

---
+++ inventory.php
@@ -0,0 +1,44 @@
+<?php
+include_once('navbar.php');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>England Discs</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Section-->
+        <section class="h-100" style="background-color: #eee;">
+            <div class="container h-100 py-5">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <h1 class="justify-content-start fw-normal mb-0 text-black">Inventory Management System</h1>
+                    <!-- <div class="col-10">
+                        <div class="d-flex justify-content-between align-items-center mb-4">
+                            <div>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; England Discs 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ protected/UserDB.php
@@ -0,0 +1,104 @@
+<?php 
+require_once('config.inc.php');
+
+class UserDB{
+
+    private static $connection;
+    private static $settings = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    );
+
+
+    function __construct(){
+        if (!isset(self::$connection)) {
+            try {
+                self::$connection = new PDO(
+                        DBCONNSTRING,
+                        DBUSER,
+                        DBPASS,
+                        self::$settings
+                );
+            } catch (PDOException $e) {
+                throw new PDOException($e->getMessage(), (int) $e->getCode());
+            }
+        }
+    }
+
+    public static function register($firstName, $lastName, $email, $password){
+        try{
+            $sql = "SELECT id, firstName, lastName FROM profiles WHERE email = :email";
+            $statement = self::$connection->prepare($sql);
+            $statement->bindParam(':email', $email);
+            $statement->execute();
+            $result = $statement->fetchColumn();
+
+            if ($result > 0) {
+                // Username already exists
+                echo 'Username exists, please choose another!';
+            } else {
+                // Username doesn't exist, insert new account
+                $pass = password_hash($password, PASSWORD_BCRYPT);
+                $sql = "INSERT INTO profiles (firstName, lastName, email, password) VALUES (:firstName, :lastName, :email, '$pass')";
+                $statement = self::$connection->prepare($sql);
+
+                $statement->bindParam(':firstName', $firstName);
+                $statement->bindParam(':lastNamme', $lastName);
+                $statement->bindParam(':email', $email);
+
+                $statement->execute();
+                echo 'You have successfully registered, you can now login!';
+            }
+        }
+        catch(PDOException $e){
+            echo 'Could not prepare statement!';
+            die( $e->getMessage() );
+        }
+        finally {
+            $pdo = null;
+        }
+    }
+
+    public static function login($email, $password){
+        try{
+            $sql = "SELECT id, firstName, lastName, email, password FROM profiles WHERE email = :email";
+            $statement = self::$connection->prepare($sql);
+            $statement->bindParam(':email', $email);
+            $statement->execute();
+            $result = $statement->fetch();
+
+            if ($result != NULL) {
+                // Username already exists
+                if(password_verify($password, $result['password'])){
+                    // Verification success! User has logged-in!
+                    // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                    session_regenerate_id();
+                    $_SESSION['id'] = $result['id'];
+                    $_SESSION['firstName'] = $result['firstName'];
+                    $_SESSION['lastName'] = $result['lastName'];
+                    $_SESSION['email'] = $result['email'];
+                    $_SESSION['loggedin'] = TRUE;
+                    return 1;
+                }else{
+                    // echo("Incorrect username or password.");
+                    return 0;
+                }
+                
+            }else{
+                // echo("Incorrect username or password.");
+                return 0;
+            }
+
+
+
+        }catch(PDOException $e){
+            echo("Incorrect username or password.");
+            die( $e->getMessage() );
+        }
+        finally {
+            $pdo = null;
+        }
+    }
+}
+
+?>
